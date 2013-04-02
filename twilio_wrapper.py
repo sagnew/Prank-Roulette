ba9baf00d7ab4d12be5c8941b42f007899c1a8f0
Sam Agnew <sagnew@eden.rutgers.edu>
2013-04-02 18:18:43 -0400

    Accidentally had 2 space tabs

--- twilio_wrapper.py
+++ twilio_wrapper.py
@@ -11,26 +11,26 @@ states = ['+14846854944', '+14159686840']
 
 
 def setUpCall(from_state, numbers, room):
-  if len(numbers) < 2 or not 0 <= from_state < 2:
-    print "error: not enough numbers for conference"
+    if len(numbers) < 2 or not 0 <= from_state < 2:
+        print "error: not enough numbers for conference"
         return -1
     global client
 
     for num in numbers:
-      #num = cleanUpNum(num)
+        #num = cleanUpNum(num)
         call = client.calls.create(to="+1" + num,
-            from_=states[from_state],
-            url="http://twimlets.com/conference?Name=" +
-            room + "&Message=%20")
+                from_=states[from_state],
+                url="http://twimlets.com/conference?Name=" +
+                room + "&Message=%20")
 
         return call.sid
 
 
 def killCall(call_id):
-  call = client.calls.update(call_id, status="completed")
+    call = client.calls.update(call_id, status="completed")
     print call.start_time
 
 
 def cleanUpNum(num):
-  num = num.translate(None, '()-')
+    num = num.translate(None, '()-')
     return num
