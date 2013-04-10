63accbf185d160e82cd6aedaacb723dacaedbad0
Sam Agnew <sagnew@eden.rutgers.edu>
2013-04-09 20:28:14 -0400

    Fixed text color

--- twilio_wrapper.py
+++ twilio_wrapper.py
@@ -3,8 +3,8 @@ from twilio.rest import TwilioRestClient
 import os
 
 # Get these credentials from http://twilio.com/user/account
-account_sid = int(os.environ.get("TWILIO_ACCOUNT_SID"))
-auth_token = int(os.environ.get("TWILIO_AUTH_TOKEN"))
+account_sid = "AC566da319c49345fe4fbbbea81ada1de0"
+auth_token = "741080b04bcb6c2471cf9439d939abce"
 client = TwilioRestClient(account_sid, auth_token)
 room_number = 0
 
